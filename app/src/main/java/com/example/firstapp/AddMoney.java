e2b076c9b67fcaaac69be733814548459778d241
Akash <akashsrivastava972002@gmail.com>
2023-06-09 22:08:04 +0530

    app version 1.0 released

--- app/src/main/java/com/example/firstapp/AddMoney.java
+++ app/src/main/java/com/example/firstapp/AddMoney.java
@@ -6,17 +6,23 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 public class AddMoney extends AppCompatActivity {
     Button button;
     EditText editText;
     EditText editText2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,31 +36,25 @@ public class AddMoney extends AppCompatActivity {
         button = findViewById(R.id.button);
         editText = findViewById(R.id.amount);
         editText2 = findViewById(R.id.note);
+        button.setEnabled(false);
+        editText.addTextChangedListener(watch);
+        editText2.addTextChangedListener(watch);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //getting the date
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
                 String currentDate = dateFormat.format(new Date());
                 //getting the amount
                 int amt = Integer.parseInt(editText.getText().toString());
                 //getting the note
                 String nt = editText2.getText().toString();
-                if (nt.equals(""))  {
-                    Log.d("mytag", "onClick: empty1");
-                    editText2.setError("Field cannot be empty");
-                } else if (editText.getText().toString().equals("")) {
-                    editText.setError("Field cannot be empty");
-                    Log.d("mytag", "onClick: empty2");
-                } else {
-                    Log.d("mytag", "onClick: empty3");
+                TransactionList item;
+                item = new TransactionList(amt, nt, currentDate, color_name);
+                dbHelper dbhelper = new dbHelper(AddMoney.this);
+                boolean b = dbhelper.addItem(item);
 
-                    TransactionList item;
-                        item = new TransactionList(amt, nt, currentDate, color_name);
-                        dbHelper dbhelper = new dbHelper(AddMoney.this);
-                        boolean b = dbhelper.addItem(item);
-                }
                 finish();
             }
         });
@@ -64,4 +64,24 @@ public class AddMoney extends AppCompatActivity {
         else if(color_name.equals("sub"))
             constraintLayout.setBackgroundColor(Color.parseColor("#E3A5A5"));
     }
+    public TextWatcher watch = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String amt = editText.getText().toString().trim();
+            String nt = editText2.getText().toString().trim();
+
+            button.setEnabled(!amt.isEmpty() && !nt.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
 }
